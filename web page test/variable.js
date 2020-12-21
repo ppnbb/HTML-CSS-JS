10cb82315d20f33c5d8e640dc6d36b8e85b85ce0
Hoo Lee <74523555+ppnbb@users.noreply.github.com>
2020-12-21 23:01:47 +0900

    variable.js
    test

---
+++ web page test/variable.js
@@ -0,0 +1,9 @@
+var a = 1;
+console.log(a);
+a=2;
+console.log(a);
+//1=2; comment
+
+var name = 'Hi, my name is node.js.';
+var letter = 'hello my name is variable. Whats your name? '+name+' how are u?';
+console.log(letter);
